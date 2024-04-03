1f30a0907485e4e961f781308ed0542ff4cd4a18
kcherechecha <kcherechecha@gmail.com>
2024-04-03 17:43:58 +0300

    entities added

---
+++ MovieCatalog.DAL/Data/Entities/Film.cs
@@ -0,0 +1,18 @@
+﻿using MovieCatalog.DAL.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalog.DAL.Data.Entitie
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public DateTime Release { get; set; }
+        public virtual ICollection<FilmCategory> FilmCategories { get; set; }
+    }
+}
---
+++ MovieCatalog.DAL/Data/Entities/FilmCategory.cs
@@ -0,0 +1,18 @@
+﻿using MovieCatalog.DAL.Data.Entitie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalog.DAL.Data.Entities
+{
+    public class FilmCategory
+    {
+        public int Id { get; set; }
+        public int FilmId {  get; set; }
+        public Film Film { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
