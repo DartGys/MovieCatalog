fe1300712130462a48e4b3bb0fbe1666c77cebb3
kcherechecha <kcherechecha@gmail.com>
2024-04-04 13:05:24 +0300

    service intarface

---
+++ MovieCatalog.BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MovieCatalog.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalog.BLL.Interfaces
+{
+    public interface ICategoryService : ICrud<CategoryModel>
+    {
+    }
+}
---
+++ MovieCatalog.BLL/Interfaces/ICrud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalog.BLL.Interfaces
+{
+    public interface ICrud<TModel> where TModel : class
+    {
+        Task<IEnumerable<TModel>> GetAllAsync();
+
+        Task<TModel> GetByIdAsync(int id);
+
+        Task<Guid> AddAsync(int model);
+
+        Task<TModel> UpdateAsync(TModel model);
+
+        Task DeleteAsync(int modelId);
+    }
+}
