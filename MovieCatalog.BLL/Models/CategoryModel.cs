dfe38ea3aae53f5425bee59130db9793e9124bda
kcherechecha <kcherechecha@gmail.com>
2024-04-04 12:37:08 +0300

    business models

---
+++ MovieCatalog.BLL/Models/CategoryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalog.BLL.Models
+{
+    public class CategoryModel
+    {
+        public CategoryModel(int id, string name, int filmsCount, IReadOnlyList<CategoryModel> categories)
+        {
+            Id = id;
+            Name = name;
+            FilmsCount = filmsCount;
+            Categories = categories;
+
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FilmsCount { get; set; }
+        public IReadOnlyList<CategoryModel> Categories { get; set; }
+    }
+}
