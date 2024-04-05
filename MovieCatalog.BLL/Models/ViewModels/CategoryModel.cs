585d0cd729ca9e64209e9b56f836e3f852e94d79
kcherechecha <kcherechecha@gmail.com>
2024-04-05 14:45:17 +0300

    category model fix

--- MovieCatalog.BLL/Models/ViewModels/CategoryModel.cs
+++ MovieCatalog.BLL/Models/ViewModels/CategoryModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MovieCatalog.BLL.Models.ViewModels
@@ -14,6 +15,7 @@ namespace MovieCatalog.BLL.Models.ViewModels
         }
         public string Name { get; set; }
         public int FilmsCount { get; set; }
+        [JsonIgnore]
         public CategoryModel? ParentCategory { get; set; }
         public virtual IReadOnlyList<CategoryModel>? ChildCategories { get; set; }
     }
