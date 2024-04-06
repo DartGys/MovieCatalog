4caee242a33d90a1d314464a96283e8f8249ad30
kcherechecha <kcherechecha@gmail.com>
2024-04-06 13:56:01 +0300

    category view model + api url

---
+++ MovieCatalog.WebUI/Models/CategoryVm.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieCatalog.WebUI.Models
+{
+    public class CategoryVm
+    {
+        public string Name { get; set; }
+        public int FilmsCount { get; set; }
+        public virtual IReadOnlyList<CategoryVm>? ChildCategories { get; set; }
+    }
+}
