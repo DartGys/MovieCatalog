7eb6fd05ccfcd8545d38249d819e2b706c8e9d04
kcherechecha <kcherechecha@gmail.com>
2024-04-06 20:07:03 +0300

    film controller + models

---
+++ MovieCatalog.WebUI/Models/FilmDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieCatalog.WebUI.Models
+{
+    public class FilmDto
+    {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public DateTime Release { get; }
+    }
+}
---
+++ MovieCatalog.WebUI/Models/FilmVm.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieCatalog.WebUI.Models
+{
+    public class FilmVm
+    {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public DateTime Release { get; set; }
+        public IReadOnlyList<string>? Categories { get; set; }
+    }
+}
