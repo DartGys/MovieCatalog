bb4f7e99561c4fc9040d6f0d36d20f0f9511f19f
kcherechecha <kcherechecha@gmail.com>
2024-04-06 15:05:24 +0300

    category controller

---
+++ MovieCatalog.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieCatalog.WebUI.Models;
+
+namespace MovieCatalog.WebUI.Controllers
+{
+    [Route("[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly string? url;
+        private readonly HttpClient client;
+
+        public CategoryController(IConfiguration config)
+        {
+            url = config.GetValue<string>("ApiSettings:ApiUrl") + "category";
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var categories = await client.GetFromJsonAsync<IEnumerable<CategoryVm>>("");
+
+            return View(categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryDto category)
+        {
+            var response = await client.PatchAsJsonAsync("", category);
+
+            if(response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(response);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await client.DeleteAsync($"{id}");
+
+            if(response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(response);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CategoryDto category)
+        {
+            var response = await client.PutAsJsonAsync("", category);
+
+            if(response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(response);
+            }
+        }
+    }
+}
---
+++ MovieCatalog.WebUI/Models/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieCatalog.WebUI.Models
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; }
+        public int? ParentCategoryId { get; set; }
+    }
+}
